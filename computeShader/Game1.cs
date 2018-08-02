ef6b799c877069c224fa113a2272b9bebac940dc
nerogann <42002172+nerogann@users.noreply.github.com>
2018-08-02 09:09:15 +0800

    master

---
+++ computeShader/Game1.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+
+namespace computeShader
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    /// 
+    public struct ResultData
+    {
+        public float functionResult;
+        public int x;
+        public float unused;
+        public float unused2;
+
+        public override string ToString()
+        {
+            return string.Format("X: {0} Y: {1}", x, functionResult);
+        }
+    }
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+
+        string debug;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            Content.RootDirectory = "Content";
+
+        }
+        static double FACT(int n)
+        {
+            double tot = 1.0;
+            while (n > 1)
+            {
+                tot *= (float)n;
+                n--;
+            }
+            return tot;
+        }
+
+        static float MacLaurin(float x)
+        {
+            float tot = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                tot += (float)(Math.Pow(x, i) / FACT(i));
+            }
+            return tot;
+        }
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+
+            int repetition = (64 * 32) * (64 * 32);
+            SharpComputeShader<ResultData> computer = new SharpComputeShader<ResultData>(graphics.GraphicsDevice, "HLSL.txt", "CS", repetition);
+
+            Console.WriteLine("Executing Mac Laurin Series of Sin(x)");
+            Console.WriteLine("With x that go from 0 to " + repetition);
+            Console.WriteLine();
+
+            //Start Compute Shader Algorithm
+            debug += "\nCompute Shader Algorithm";
+
+            //start timer
+            Stopwatch st = new Stopwatch();
+            st.Start();
+
+            //execute compute shader
+            computer.Begin();
+            computer.Start(64, 64, 1);
+            computer.End();
+
+            //stop timer
+            st.Stop();
+
+            //get result
+            ResultData[] data = computer.ReadData(repetition);
+
+
+            int csTime = (int)st.ElapsedMilliseconds;
+
+            debug += "\n"+string.Format("Time: {0} ms", csTime);
+
+            //Start CPU Algorithm
+            debug += "\n\nCPU Algorithm";
+            float[] values = new float[repetition];
+            st.Start();
+            for (int i = 0; i < repetition; i++)
+            {
+                values[i] = MacLaurin(i / 1000.0F);
+            }
+            st.Stop();
+            int cpuTime = (int)st.ElapsedMilliseconds;
+            debug += "\n" + string.Format("Time: {0} ms", cpuTime);
+
+
+            debug += "\n\nCheck Sample Results";
+
+            for (int i = 1; i < 10; i++)
+            {
+                int x = i * 10000;
+                debug += "\n"+string.Format("TEST {0} ComputeShader: {1}    |   CPU: {2} ", i, data[x].functionResult, values[x]);
+            }
+
+            
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("font");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, debug , new Vector2(5,5), Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ computeShader/SharpComputeShader.cs
@@ -0,0 +1,102 @@
+﻿using computeShader.Extension;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D11;
+
+using Buffer11 = SharpDX.Direct3D11.Buffer;
+
+namespace computeShader
+{
+    public class SharpComputeShader<T> where T : struct
+    {
+
+        private Device _d3dDevice;
+        private DeviceContext _d3dContext;
+        private UnorderedAccessView _accessView;
+        private ComputeShader _shader;
+
+        private Buffer11 _buffer;
+        private Buffer11 _resultBuffer;
+
+        public SharpComputeShader(Microsoft.Xna.Framework.Graphics.GraphicsDevice MNGDevice, string filename, string functionName, int count)
+        {
+            _d3dDevice = (MNGDevice.Handle as SharpDX.Direct3D11.Device);
+            _d3dContext = (MNGDevice.Handle as SharpDX.Direct3D11.Device).ImmediateContext;
+            
+
+            _accessView = CreateUAV(count, out _buffer);
+            _resultBuffer = CreateStaging(count);
+
+
+            var computeShaderByteCode = ShaderBytecode.CompileFromFile(filename, functionName, "cs_5_0");
+            _shader = new ComputeShader(_d3dDevice, computeShaderByteCode);
+        }
+
+        private UnorderedAccessView CreateUAV(int count, out Buffer11 buffer)
+        {
+            int size = SharpDX.Utilities.SizeOf<T>();
+            BufferDescription bufferDescription = new BufferDescription()
+            {
+                BindFlags = BindFlags.UnorderedAccess | BindFlags.ShaderResource,
+                Usage = ResourceUsage.Default,
+                CpuAccessFlags = CpuAccessFlags.None,
+                OptionFlags = ResourceOptionFlags.BufferStructured,
+                StructureByteStride = size,
+                SizeInBytes = size * count
+            };
+
+            buffer = new Buffer11(_d3dDevice, bufferDescription);
+
+
+            UnorderedAccessViewDescription uavDescription = new UnorderedAccessViewDescription()
+            {
+                Buffer = new UnorderedAccessViewDescription.BufferResource() { FirstElement = 0, Flags = UnorderedAccessViewBufferFlags.None, ElementCount = count },
+                Format = SharpDX.DXGI.Format.Unknown,
+                Dimension = UnorderedAccessViewDimension.Buffer
+            };
+
+            return new UnorderedAccessView(_d3dDevice, buffer, uavDescription);
+
+        }
+        private Buffer11 CreateStaging(int count)
+        {
+            int size = SharpDX.Utilities.SizeOf<T>() * count;
+            BufferDescription bufferDescription = new BufferDescription()
+            {
+                SizeInBytes = size,
+                BindFlags = BindFlags.None,
+                CpuAccessFlags = CpuAccessFlags.Read | CpuAccessFlags.Write,
+                Usage = ResourceUsage.Staging,
+                OptionFlags = ResourceOptionFlags.None,
+            };
+
+            return new Buffer11(_d3dDevice, bufferDescription);
+        }
+
+        public void Begin()
+        {
+            _d3dContext.ComputeShader.SetUnorderedAccessView(0, _accessView);
+            _d3dContext.ComputeShader.Set(_shader);
+        }
+        public void Start(int threadGroupCountX, int threadGroupCountY, int threadGroupCountZ)
+        {
+            _d3dContext.Dispatch(threadGroupCountX, threadGroupCountY, threadGroupCountZ);
+        }
+
+        public void End()
+        {
+            _d3dContext.CopyResource(_buffer, _resultBuffer);
+            _d3dContext.Flush();
+            _d3dContext.ComputeShader.SetUnorderedAccessView(0, null);
+            _d3dContext.ComputeShader.Set(null);
+        }
+
+        public T[] ReadData(int count)
+        {
+            SharpDX.DataStream stream;
+            SharpDX.DataBox box = _d3dContext.MapSubresource(_resultBuffer, 0, MapMode.Read, MapFlags.None, out stream);
+            T[] result = stream.ReadRange<T>(count);
+            _d3dContext.UnmapSubresource(_buffer, 0);
+            return result;
+        }
+    }
+}
